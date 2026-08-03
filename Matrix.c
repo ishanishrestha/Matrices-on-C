f3f1cb111c0023baf552ad217af5600e3e8bb591
ishanishrestha <ishanishr08@gmail.com>
2026-08-03 16:57:26 +0545

    Update Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -8,7 +8,11 @@ int main()
          for (j=0;j<3;j++)
          {
              printf("Enter row %d and column %d element: ", i+1, j+1);
-             scanf("%d",&a[i][j]);
+             while (scanf("%d", &a[i][j]) != 1) 
+             {
+                 printf("Invalid! Only numbers are acccepted! Enter a number: ");
+                 scanf("%*s");
+             }
              }
         }
         printf ("\nThe matrix is:");
