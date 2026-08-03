e2f027c7c2f52d4d79a84b25d2eb5c91897c1692
ishanishrestha <ishanishr08@gmail.com>
2026-08-03 19:33:12 +0545

    Updated

--- TransposeofMatrix.c
+++ TransposeofMatrix.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-     int i,j,a[2][3];
+     int i,j,a[3][2];
      printf ("Matrix: \n\n");
-     for (i=0;i<2;i++)
+     for (i=0;i<3;i++)
      {
-         for (j=0;j<3;j++)
+         for (j=0;j<2;j++)
          {
              printf("Enter row %d and column %d element: ", i+1, j+1);
              while (scanf("%d", &a[i][j]) != 1) 
@@ -15,13 +15,22 @@ int main()
              }
              }
         }
-        printf ("\nThe matrix is:");
+        printf ("\nThe matrix is:\n");
+        for (i=0;i<3;i++)
+     {
+         for (j=0;j<2;j++)
+         {
+         printf("%d\t",a[i][j]);
+         }
+         printf("\n");
+        }
+        printf ("\n\nThe transpose of this matrix is:");
         printf ("\n");
      for (i=0;i<2;i++)
      {
          for (j=0;j<3;j++)
          {
-         printf("%d\t",a[i][j]);
+         printf("%d\t",a[j][i]);
          }
          printf("\n");
         }
