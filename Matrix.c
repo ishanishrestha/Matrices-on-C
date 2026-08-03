4bd57c5130bffbf94d8cd72656317431551f9b21
ishanishrestha <ishanishr08@gmail.com>
2026-08-03 10:42:41 +0545

    New

---
+++ Matrix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+     int i,j,a[2][3];
+     printf ("Matrix \n");
+     for (i=0;i<2;i++)
+     {
+         for (j=0;j<3;j++)
+         {
+             printf("Enter elements: ");
+             scanf("%d",&a[i][j]);
+             }
+             }
+     for (i=0;i<2;i++)
+     {
+         for (j=0;j<3;j++)
+         {
+         printf("%d\t",a[i][j]);
+         }
+         printf("\n");
+         }
+     return 0;
+}
+//comment
